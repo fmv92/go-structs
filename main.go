71d7671688c05c99ebccdf8fe53b1e49177ff5aa
Fernando <fernando.mata.villalobos@gmail.com>
2023-01-23 10:10:22 -0600

    Setting and updating struct values

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+
+	//spike := person{"Spike", "Spiegel"}
+	// spike := person{
+	// 	firstName: "Spike",
+	// 	lastName:  "Spiegel"}
+	var spike person
+	spike.firstName = "Spike"
+	spike.lastName = "Spiegel"
+
+	fmt.Printf("%+v", spike)
 
 }
