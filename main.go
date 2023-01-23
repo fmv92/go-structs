548e7c80b9ec52ac507ec001dde9601cf20cfe0f
Fernando <fernando.mata.villalobos@gmail.com>
2023-01-23 10:21:18 -0600

    Embedding structs

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact contactInfo
+}
+
+type contactInfo struct {
+	email string
+	zip int
 }
 
 func main() {
 
 	//spike := person{"Spike", "Spiegel"}
-	// spike := person{
-	// 	firstName: "Spike",
-	// 	lastName:  "Spiegel"}
+	faye := person{
+		firstName: "Faye",
+		lastName:  "Valentine",
+		contact: contactInfo{
+			email: "faye@gmail.com",
+			zip: 31000, 
+		},
+	}
 	var spike person
 	spike.firstName = "Spike"
 	spike.lastName = "Spiegel"
+	spike.contact.email = "bebop@gmail.com"
+	spike.contact.zip = 31150
 
-	fmt.Printf("%+v", spike)
+	fmt.Printf("%+v \n", spike)
+	fmt.Printf("%+v", faye)
 
 }
